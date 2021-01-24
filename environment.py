0aca0bf6af3d240414317ea0a1b24cc984e21b7d
Xavier Canal i Masjuan <xeviknal@gmail.com>
2021-01-24 01:31:57 +0100

    Adapting to carpole lab

--- environment.py
+++ environment.py
@@ -1,12 +1,6 @@
-from helpers import wrap_env
 import gym
-import random
-import torch
-import math
 
-
-def compute_eps_threshold(step, eps_start, eps_end, eps_decay):
-    return eps_end + (eps_start - eps_end) * math.exp(-1. * step / eps_decay)
+from helpers import wrap_env
 
 
 class CarRacingEnv:
@@ -15,36 +9,32 @@ class CarRacingEnv:
         super().__init__()
         self.env = wrap_env(gym.make("CarRacing-v0"))
         self.total_rew = 0
-        self.ob = None
+        self.state = None
         self.done = False
         self.device = device
 
+    def step(self, action):
+        return self.env.step(action)
+
     def reset(self):
-        self.ob, self.done, self.total_rew = self.env.reset(), False, 0
+        self.state, self.done, self.total_rew = self.env.reset(), False, 0
 
     def rand_episode_run(self):
         while not self.done:
             self.env.render()
-            ac = self.env.action_space.sample()
-            self.ob, rew, self.done, info = self.env.step(ac)
+            ac = self.select_action(self.state)
+            self.state, rew, self.done, info = self.env.step(ac)
+            print(self.state)
+            print(self.state.shape)
             self.total_rew += rew
 
     def print_reward(self):
         print('Total Reward obtained: {0}'.format(self.total_rew))
 
+    def spec(self):
+        return self.env.spec
+
     def close(self):
         self.close()
 
-    def select_action(self, policy, state, eps_greedy_threshold, n_actions):
-        # e-greedy strategy
-        if random.random() > eps_greedy_threshold:
-            with torch.no_grad():
-                # t.max(1) will return largest column value of each row.
-                # second column on max result is index of where max element was
-                # found, so we pick action with the larger expected reward.
-                action = policy(state).max(1)[1].view(1, 1)
-        else:
-            action = torch.tensor(
-                [[random.randrange(n_actions)]], device=self.device, dtype=torch.long)
-        return action
 
---
+++ policy.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+
+
+class Policy(nn.Module):
+
+    def __init__(self, inputs=3, outputs=8):
+        super(Policy, self).__init__()
+        self.pipeline = nn.Sequential(
+            nn.Conv2d(inputs, 32, 3),  # [32, 94, 94]
+            nn.ReLU(),
+            nn.MaxPool2d(2),  # [32, 47, 47]
+            nn.Conv2d(32, 64, 4),  # [64, 44, 44]
+            nn.ReLU(),
+            nn.MaxPool2d(2),  # [64, 22, 22]
+            nn.Flatten(),
+            nn.Linear(64 * 22 * 22, 512),
+            nn.ReLU(),
+            nn.Linear(512, outputs),
+            nn.LogSoftmax(dim=-1)
+        )
+        self.saved_log_probs = []
+        self.rewards = []
+
+    def forward(self, x):
+        return self.pipeline(x)
