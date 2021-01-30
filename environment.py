b7c29d0f6506d11c81e87d6ea0799b131bef65af
Xavier Canal i Masjuan <xeviknal@gmail.com>
2021-01-30 23:51:34 +0100

    Better hyperparam tunning prep

--- environment.py
+++ environment.py
@@ -5,7 +5,7 @@ from gym.wrappers import FrameStack, GrayScaleObservation
 
 class CarRacingEnv:
 
-    def __init__(self, device):
+    def __init__(self, device, stack_frames=4):
         super().__init__()
         self.total_rew = 0
         self.state = None
@@ -14,7 +14,7 @@ class CarRacingEnv:
 
         self.env = gym.make("CarRacing-v0")
         self.env = GrayScaleObservation(self.env)
-        self.env = FrameStack(self.env, 4)
+        self.env = FrameStack(self.env, stack_frames)
         self.env = FrameSkipper(self.env, 4)
         print(self.env.observation_space)
 
@@ -23,15 +23,6 @@ class CarRacingEnv:
 
     def reset(self):
         self.state, self.done, self.total_rew = self.env.reset(), False, 0
-
-    def rand_episode_run(self):
-        while not self.done:
-            self.env.render()
-            ac = self.select_action(self.state)
-            self.state, rew, self.done, info = self.env.step(ac)
-            print(self.state)
-            print(self.state.shape)
-            self.total_rew += rew
 
     def print_reward(self):
         print('Total Reward obtained: {0}'.format(self.total_rew))
