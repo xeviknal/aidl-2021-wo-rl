20fedd2bec779bcc215435ce5d859c99787009e1
Xavier Canal i Masjuan <xeviknal@gmail.com>
2021-02-02 23:16:34 +0100

    Adding juanjo modification

--- actions.py
+++ actions.py
@@ -1,14 +1,18 @@
 class Actions:
 
     available_actions = [
-        [0.0, 0.5, 0.0],  # Need for speed throttle
-        [0.0, 0.2, 0.0],  # Grandpa throttle
-        [0.0, 0.2, 0.6],  # Hard break
-        [0.0, 0.2, 0.6],  # Soft break
-        [-0.8, 0.2, 0.0],  # Hard left
-        [-0.3, 0.1, 0.0],  # Soft left
-        [0.8, 0.2, 0.0],  # Hard right
-        [0.3, 0.1, 0.0],  # Soft right
+        [0.0, 0.7, 0.0],  # throttle
+        [0.0, 0.5, 0.0],  # throttle
+        [0.0, 0.2, 0.0],  # throttle
+        [0.0, 0.0, 0.7],  # break
+        [0.0, 0.0, 0.5],  # break
+        [0.0, 0.0, 0.2],  # break
+        [-0.8, 0.1, 0.0],  # left
+        [-0.5, 0.1, 0.0],  # left
+        [-0.2, 0.1, 0.0],  # left
+        [0.8, 0.1, 0.0],  # right
+        [0.5, 0.1, 0.0],  # right
+        [0.2, 0.1, 0.0],  # right
     ]
 
     def __class_getitem__(cls, item):
