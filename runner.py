55b95eab4414d1d0ce64cae00ad721b8e936e580
jpedret <j.pedret.pinilla@gmail.com>
2021-03-18 16:57:28 +0100

    runner.py includes heatmap mp4 code

--- runner.py
+++ runner.py
@@ -4,6 +4,10 @@ import numpy as np
 from policy import Policy
 from actions import available_actions
 
+import seaborn as sns
+import matplotlib.pyplot as plt
+import os
+
 class Runner:
     def __init__(self, env, config):
         super().__init__()
@@ -25,15 +29,25 @@ class Runner:
         # We pick the action from a sample of the probabilities
         # It prevents the model from picking always the same action
         m = torch.distributions.Categorical(probs)
+        eprobs = torch.exp(torch.squeeze(probs)).detach().numpy()
         action = m.sample()
-        return available_actions[action.item()]
+        return available_actions[action.item()], eprobs
 
     def run(self):
         state, done, total_rew = self.env.reset(), False, 0
-        while not done:
+        eprobs_list = []
+        i = 0
+        while not done and i<50:
             self.env.render()
-            action = self.select_action(state)
+            action, eprobs = self.select_action(state)
             state, rew, done, info = self.env.step(action)
             total_rew += rew
+            eprobs_list.append(eprobs)
+            eprobs_list_T = list(map(list, zip(*eprobs_list)))
+            plt.figure()
+            heatmap = sns.heatmap(eprobs_list_T, vmin=0, vmax=1, cmap="Blues")
+            plt.savefig("./heatmap/heatmap_{}".format(i))
+            i = i +1
+        os.system("ffmpeg -r 20 -i ./heatmap/heatmap_%01d.png -vcodec mpeg4 -y ./heatmap/heatmap.mp4")
         print('Cumulative reward:', total_rew)
         self.env.close()
