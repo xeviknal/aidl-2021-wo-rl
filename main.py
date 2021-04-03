19187d4e329fb995a98751601f79e4834382e741
jpedret <j.pedret.pinilla@gmail.com>
2021-04-03 09:22:49 +0200

    reinforcement 71k / different set of actions test

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 if __name__ == "__main__":
     hyperparams = {
-        'num_episodes': 40000,  # Number of training episodes
+        'num_episodes': 150000,  # Number of training episodes
         'lr': 1e-3,  # Learning rate
         'gamma': 0.99,  # Discount rate
         'log_interval': 10,  # controls how often we log progress
