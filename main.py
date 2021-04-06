d3dda5a319a7bcb62e0e970dc7d809aedf45e0b1
jpedret <j.pedret.pinilla@gmail.com>
2021-04-06 12:01:22 +0200

    Run up to 33.7k episodes using actions set 3 - max/final av. reward 881/825

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 if __name__ == "__main__":
     hyperparams = {
-        'num_episodes': 20000,  # Number of training episodes
+        'num_episodes': 200000,  # Number of training episodes
         'lr': 1e-3,  # Learning rate
         'gamma': 0.99,  # Discount rate
         'log_interval': 10,  # controls how often we log progress
