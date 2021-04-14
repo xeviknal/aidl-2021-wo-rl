dfaadca2df470fdb23f317a3e92c5ea192354912
Xavier Canal i Masjuan <xeviknal@gmail.com>
2021-04-15 00:17:03 +0200

    Change experiment name

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def train(config):
 
 
 # for concurrent runs and logging
-experiment = 'ppo-nm-hp-tuning'
+experiment = 'ppo-nm-hp-tuning-2'
 if __name__ == "__main__":
     hyperparams = {
         'num_epochs': 2000,  # Number of training episodes
