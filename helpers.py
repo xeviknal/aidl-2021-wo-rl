fcf3ef90309932ea857841435922fc94926672c3
Xavier Canal i Masjuan <xeviknal@gmail.com>
2021-04-16 00:04:16 +0200

    Allow to define hyperparams via process params

--- helpers.py
+++ helpers.py
@@ -22,6 +22,15 @@ def show_video():
         print("Could not find video")
 
 
+def print_hyperparams(config):
+    print('----------------------------------------')
+    print("{:<15} {:<10}".format('Param', 'Value'))
+    for label, value in config.items():
+        if label is not "device":
+            print("{:<15} {:<10}".format(label, value))
+    print('----------------------------------------')
+
+
 def display_start():
     display = Display(visible=0, size=(1400, 900))
     display.start()
@@ -29,6 +38,7 @@ def display_start():
 
 def save_model(model, path):
     torch.save(model.state_dict(), path)
+
 
 def create_directory(path):
     try:
