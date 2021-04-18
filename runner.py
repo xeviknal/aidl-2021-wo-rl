ab8b84269919c2507048f5208b7ff609c8fd323f
Xavier Canal i Masjuan <xeviknal@gmail.com>
2021-04-18 12:28:29 +0200

    Importing heatmap

--- runner.py
+++ runner.py
@@ -1,6 +1,10 @@
+import os
 import torch
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+import helpers
 from actions import get_action
 from trainers.factory import get_strategy_descriptor
 
@@ -15,10 +19,12 @@ class Runner:
         super().__init__()
         self.env = env
         self.config = config
+        self.build_heatmap = config['heatmap']
         self.input_channels = config['stack_frames']
         self.action_set = get_action(config['action_set_num'])
         self.policy = policy_class(self.input_channels, len(self.action_set))
         self.policy.load_checkpoint(config['params_path'])
+        self.heatmap_probs = []
 
     def select_action(self, state):
         if state is None:  # First state is always None
@@ -32,14 +38,37 @@ class Runner:
         # It prevents the model from picking always the same action
         m = torch.distributions.Categorical(probs)
         action = m.sample()
-        return self.action_set[action.item()]
+        return self.action_set[action.item()], probs
+
+    def heatmap_build(self, iteration, step_probs):
+        self.heatmap_probs.append((torch.squeeze(step_probs)).detach().numpy())
+        probs_list_t = list(map(list, zip(*self.heatmap_probs)))
+        plt.figure()
+        sns.heatmap(probs_list_t, vmin=0, vmax=1, cmap="Blues")
+        plt.savefig("./heatmap/heatmap_{}".format(iteration))
+        plt.close()
+
+    def compose_heatmap(self):
+        os.system("ffmpeg -r 50 -i ./heatmap/heatmap_%01d.png -vcodec mpeg4 -y ./heatmap/heatmap.mp4")
+        os.system("ffmpeg -i ./heatmap/heatmap.mp4 -vf scale=600:400 -strict -2 ./heatmap/heatmap_scale.mp4")
+        os.system("ffmpeg -i ./video/*.mp4 -i ./heatmap/heatmap_scale.mp4 -filter_complex hstack ./video/car_and_heatmap.mp4")
 
     def run(self):
-        state, done, total_rew = self.env.reset(), False, 0
+        if self.build_heatmap:
+            helpers.create_directory('heatmap')
+
+        state, done, total_rew, i = self.env.reset(), False, 0, 0
         while not done:
             self.env.render()
-            action = self.select_action(state)
+            action, probs = self.select_action(state)
             state, rew, done, info = self.env.step(action)
             total_rew += rew
+            if self.build_heatmap:
+                self.heatmap_build(i, probs)
+            i += 1
+
+        if self.build_heatmap:
+            self.compose_heatmap()
+
         print('Cumulative reward:', total_rew)
         self.env.close()
