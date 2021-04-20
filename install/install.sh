5c59b83acf9e510f0933a6fc685f880ef63905a2
Xavier Canal i Masjuan <xeviknal@gmail.com>
2021-04-20 09:09:35 +0200

    Update install steps to use non-leaky environment (#72)

--- install/install.sh
+++ install/install.sh
@@ -20,4 +20,5 @@ cd idl-2021-wo-rl
 conda create --name car-racing python=3.8
 conda activate car-racing
 
-pip install -r requirements
+pip install -r requirements.txt
+pip install git+https://github.com/xeviknal/gym.git@master
